e8a19988686f3da9891b69b7ebe5d6657102625b
Remco Beckers <remco.beckers@gmail.com>
2015-07-30 07:45:03 +0200

    Apply transaction filter always before application filters to avoid filtering out rows that are being changed in another transaction.

--- tephra-hbase-compat-0.96/src/main/java/co/cask/tephra/hbase96/Filters.java
+++ tephra-hbase-compat-0.96/src/main/java/co/cask/tephra/hbase96/Filters.java
@@ -24,13 +24,13 @@ import org.apache.hadoop.hbase.filter.FilterList;
  */
 public final class Filters {
   /**
-   * Adds {@code overrideFilter} on to {@code baseFilter}, if it exists, otherwise replaces it.
+   * Adds {@code overrideFilter} before {@code baseFilter}, if it exists, otherwise replaces it.
    */
   public static Filter combine(Filter overrideFilter, Filter baseFilter) {
     if (baseFilter != null) {
       FilterList filterList = new FilterList(FilterList.Operator.MUST_PASS_ALL);
-      filterList.addFilter(baseFilter);
       filterList.addFilter(overrideFilter);
+      filterList.addFilter(baseFilter);
       return filterList;
     }
     return overrideFilter;
